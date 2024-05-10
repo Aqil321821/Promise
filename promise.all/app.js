d2fd142dd5fb0161f9bb540e865c7d1e972d33c2
Aqil321821 <aqilrao321821@gmail.com>
2024-05-10 22:42:59 +0500

    promise.all
    use of promise.all() method

---
+++ promise.all/app.js
@@ -0,0 +1,36 @@
+//use of promise.all
+
+
+function getData(endpoint) {
+    return new Promise((resolve, reject) => {
+        const xhr = new XMLHttpRequest();
+        xhr.open('GET', endpoint);
+        xhr.onreadystatechange = function () {
+            if (this.readyState === 4) {
+                if (this.status === 200) {
+                    resolve(JSON.parse(this.responseText));
+                } else {
+                    reject('Error: Something went wrong');
+                }
+            }
+        };
+        setTimeout(() => {
+            xhr.send();
+        }, Math.floor(Math.random() * 3000) + 1000);
+    });
+}
+
+//Now, let's store each promise response in a variable:
+
+const moviesPromise = getData('./movies.json');
+const actorsPromise = getData('./actors.json');
+const directorsPromise = getData('./directors.json');
+
+
+//now pass these promise  to promise.all()
+
+Promise.all([moviesPromise, actorsPromise, directorsPromise])
+    .then((data) => {
+        console.log(data);
+    })
+    .catch((error) => console.log(error));
---
+++ promise.all/promisAll.md
@@ -0,0 +1,13 @@
+# Handle Multiple Promises with Promise.all()
+
+There may be some instances where you want to handle multiple promises at the same time. For example, we may want to get data from multiple endpoints at the same time..
+
+
+
+Instead of chaining multiple then methods, we can use Promise.all() to handle all of the promises at the same time. We will pass in an array of promises and then we can use the then method to
+handle the response...
+
+
+SEE the code .....
+in which we fetch data from json files using promise.all() 
+
